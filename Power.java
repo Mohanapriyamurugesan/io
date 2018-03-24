25bfdac556ad419da2f313cfedf8c7cae9cd27e1
Mohanapriyamurugesan <32103124+Mohanapriyamurugesan@users.noreply.github.com>
2018-03-24 18:58:41 +0530

    Add files via upload

---
+++ Power.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Power
+
+{
+    
+	public static void main(String arg[])
+    
+	{
+        
+		int result=1, p=3, 
+		while(p!=0)
+    
+	        {
+          
+		  	result=result*n;
+    
+		        p--;
+        
+		}
+       
+		 System.out.println(result);
+ 
+        }
+
+}
