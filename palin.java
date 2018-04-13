9cfd69f887bbba30f7dba505335b0d6c470f0b98
Mohanapriyamurugesan <32103124+Mohanapriyamurugesan@users.noreply.github.com>
2018-04-13 20:15:48 +0530

    Add files via upload

---
+++ palin.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Palindrome
+
+{
+    
+	public static void main(String args[]) 
+    
+	{
+       
+		int n=121, sum=0, remainder;
+       	
+		int original = n;
+ 
+	        while(n!=0)
+	
+       {
+           
+			remainder=n%10;
+          
+		        sum=sum*10+remainder;
+           
+			n=n/10;
+       
+	       }
+        
+	       if(original==sum)
+       
+	       {
+                   
+			System.out.println("Given number is palindrome");
+        
+	       }
+        
+	       else
+   
+	       {
+                   
+			System.out.println("Given number is not palindrome");
+        
+	       }
+       
+    
+	}
+
+}
