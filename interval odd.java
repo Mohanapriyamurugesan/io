6fd7cca2344f0bb67453b83ee0b30ef40b99f253
Mohanapriyamurugesan <32103124+Mohanapriyamurugesan@users.noreply.github.com>
2018-04-13 20:40:29 +0530

    Add files via upload

---
+++ interval odd.java
@@ -0,0 +1,14 @@
+public class MyClass {
+    public static void main(String args[]) 
+    {
+        int l=1, h=6;
+        for(int i=l+1; i<h; i++)
+        {
+            if(i%2!=0)
+            {
+                System.out.println(i);
+            }
+        }
+    
+    }
+}
